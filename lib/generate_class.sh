10fde8d0f0cf826082f0e61b4f44ac3edd8f2a02
Tejas Dinkar <tejas@gja.in>
2011-02-19 22:38:23 +0530

    Hid a bunch of APIs and added QXT

--- lib/generate_class.sh
+++ lib/generate_class.sh
@@ -5,7 +5,7 @@ if [ "$1" == "" ]; then
   exit 1
 fi
 
-cat > "../include/$1.h" << EOF
+cat > "$1.h" << EOF
 #ifndef `echo $1 | tr '[:lower:]' '[:upper:]'`_H
 #define `echo $1 | tr '[:lower:]' '[:upper:]'`_H
 
