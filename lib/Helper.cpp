636314215a5e3c6fe7e1567081dd80de28c3be45
Tejas Dinkar <tejas@gja.in>
2011-03-13 19:28:26 +0530

    Added support for nested menus

--- lib/Helper.cpp
+++ lib/Helper.cpp
@@ -1,15 +1,56 @@
 #include "Helper.h"
 #include <QAction>
+#include <QMenu>
+#include <QMenuBar>
 
 namespace QTestable
 {
+  void extractMenusFromAction(const QAction *action, QVariantMap &map)
+  {
+    if(action == NULL)
+      return;
+
+    map["text"] = action->text();
+    map["type"] = "action";
+  }
+
+  void extractChildren(const QObject *menu, QVariantMap &map)
+  {
+    QList<QVariant> childrenMap;
+    foreach(QObject *child, menu->children())
+      childrenMap<<Helper::extractMenus(child);
+    map["children"] = childrenMap;
+  }
+
+  void extractMenusFromMenu(const QMenu *menu, QVariantMap &map)
+  {
+    if(menu == NULL)
+      return;
+
+    map["title"] = menu->title();
+    map["type"] = "menu";
+
+    extractChildren(menu, map);
+  }
+
+  void extractMenusFromMenuBar(const QMenuBar *menu, QVariantMap &map)
+  {
+    if(menu == NULL)
+      return;
+
+    map["type"] = "menuBar";
+
+    extractChildren(menu, map);
+  }
+
   QVariantMap Helper::extractMenus(const QObject *object)
   {
     QVariantMap map;
     map["name"] = object->objectName();
 
-    const QAction *action = dynamic_cast<const QAction *>(object);
-    map["text"] = action->text();
+    extractMenusFromAction(dynamic_cast<const QAction *>(object), map);
+    extractMenusFromMenu(dynamic_cast<const QMenu *>(object), map);
+    extractMenusFromMenuBar(dynamic_cast<const QMenuBar *>(object), map);
     return map;
   }
 }
--- test/MenuHelperTest.cpp
+++ test/MenuHelperTest.cpp
@@ -1,6 +1,8 @@
 #include <QTest>
 #include "Helper.h"
 #include <QAction>
+#include <QMenu>
+#include <QMenuBar>
 
 using namespace QTestable;
 
@@ -16,6 +18,54 @@ class MenuHelperTest : public QObject
       QVariantMap map = Helper::extractMenus(&action);
       QCOMPARE (map["name"].toString(), QString("fooAction"));
       QCOMPARE (map["text"].toString(), QString("foo"));
+      QCOMPARE (map["type"].toString(), QString("action"));
+    }
+
+    void shouldBeAbleToGetTitleAndTextForAQMenu()
+    {
+      QMenu menu("foo", NULL);
+      menu.setObjectName("fooMenu");
+
+      QVariantMap map = Helper::extractMenus(&menu);
+      QCOMPARE (map["name"].toString(), QString("fooMenu"));
+      QCOMPARE (map["title"].toString(), QString("foo"));
+      QCOMPARE (map["type"].toString(), QString("menu"));
+    }
+
+    void shouldBeAbleToGetChildrenForAQMenu()
+    {
+      QMenu menu("foo", NULL);
+      QAction action1("action1", &menu);
+      QAction action2("action2", &menu);
+
+      QVariantMap map = Helper::extractMenus(&menu);
+      QList<QVariant> children = map["children"].toList();
+
+      // For some reason, it contains an action with itself
+      QCOMPARE(children[0].toMap()["type"].toString(), QString("action"));
+      QCOMPARE(children[0].toMap()["text"].toString(), QString("foo"));
+
+      QCOMPARE(children[1].toMap()["text"].toString(), QString("action1"));
+      QCOMPARE(children[2].toMap()["text"].toString(), QString("action2"));
+    }
+
+    
+    void shouldBeAbleToGetChildrenForAQMenuBar()
+    {
+      QMenuBar menu(NULL);
+      QMenu menu1("menu1", &menu);
+      QMenu menu2("menu2", &menu);
+
+      QVariantMap map = Helper::extractMenus(&menu);
+      QList<QVariant> children = map["children"].toList();
+
+      QCOMPARE (map["type"].toString(), QString("menuBar"));
+
+      // Automatically added again
+      QCOMPARE(children[0].toMap()["name"].toString(), QString("qt_menubar_ext_button"));
+
+      QCOMPARE(children[1].toMap()["title"].toString(), QString("menu1"));
+      QCOMPARE(children[2].toMap()["title"].toString(), QString("menu2"));
     }
 };
 
